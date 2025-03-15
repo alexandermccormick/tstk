1c58d1c433f5ee9815a11f5f13d297d09fb10532
Alexander McCormick <alexandermccormick@protonmail.com>
2025-03-14 21:57:29 -0700

    adds explicit return type

--- builtin_extensions/lib/array/mod.ts
+++ builtin_extensions/lib/array/mod.ts
@@ -7,7 +7,7 @@ export class XArray<T> extends Array<T> {
    * Removes the elements of an array that do not meet the condition specified in a callback function.
    * @param predicate - A function that accepts up to three arguments. The filterInPlace method calls the predicate function one time for each element in the array.
    */
-  filterInPlace(predicate: FilterPredicate<T>) {
+  filterInPlace(predicate: FilterPredicate<T>): this {
     filterInPlace(this, predicate);
     return this;
   }
